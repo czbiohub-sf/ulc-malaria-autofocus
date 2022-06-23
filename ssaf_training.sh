ebb099578e4ceb2cc352b96bacde61be6150dc8a
Axel Jacobsen <axelnj44@gmail.com>
2022-06-23 08:49:15 -0700

    sbatch training file

---
+++ ssaf_training.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=ULCMalariaSSAFTraining
+#SBATCH --time=16:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+
+echo
+echo "Single-Shot Auto Focus (SSAF) Training"
+echo
+
+env | grep "^SLURM" | sort
+
+echo
+echo "starting ssaf training..."
+echo
+
+nvidia-smi
+
+conda run python3 train.py
