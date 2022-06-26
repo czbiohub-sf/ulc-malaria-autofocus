5d11afbed5d47c8150ba572aa0d01429634be62d
Axel Jacobsen <axelnj44@gmail.com>
2022-06-26 16:55:23 -0700

    add variance to confusion data

--- visualize_trained.py
+++ visualize_trained.py
@@ -25,18 +25,23 @@ DATA_DIRS = "/hpc/projects/flexo/MicroscopyData/Bioengineering/LFM Scope/ssaf_tr
 
 
 def get_confusion_data(net, dataset, sample_size=100, device=torch.device("cpu")):
-    outputs = []
+    outputs, variances = [], []
     classes_in_order = sorted([int(c) for c in dataset.classes])
     for clss in classes_in_order:
         samples = dataset.sample_from_class(int(clss), sample_size).to(device)
         with torch.no_grad():
-            out = net(samples).mean().item()
-        outputs.append(out)
+            out = net(samples)
+            output_num = out.mean().item()
+            output_var = out.var(unbiased=True)
+        outputs.append(output_num)
+        variances.append(output_var)
 
-    return classes_in_order, outputs
+    return classes_in_order, outputs, variances
 
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
     assert len(sys.argv) == 2, "usage: ./visualize_trained.py <PATH TO PTH>"
 
     dev = torch.device("cpu")
@@ -47,9 +52,22 @@ if __name__ == "__main__":
     net.train(False)
     net.load_state_dict(model_save["model_state_dict"])
 
-    [full_dataset_subset] = get_dataset(data_dir, sample_size)
+    sample_size = 5
+    [full_dataset_subset] = get_dataset("training_data", sample_size)
     full_dataset = full_dataset_subset.dataset
-    classes_in_order, outputs = get_confusion_data(net, full_dataset, sample_size=10)
-    plt.plot(classes_in_order, classes_in_order)
-    plt.plot(classes_in_order, outputs)
+    classes_in_order, outputs, variances = get_confusion_data(
+        net, full_dataset, sample_size=sample_size
+    )
+    outputs = np.asarray(outputs)
+    variances = np.asarray(variances)
+    plt.scatter(classes_in_order, classes_in_order, s=2)
+    plt.scatter(classes_in_order, [np.round(o) for o in outputs], c="red", s=2)
+    plt.fill_between(
+        classes_in_order,
+        outputs - variances,
+        outputs + variances,
+        alpha=0.2,
+        edgecolor="#1B2ACC",
+        facecolor="#089FFF",
+    )
     plt.show()
