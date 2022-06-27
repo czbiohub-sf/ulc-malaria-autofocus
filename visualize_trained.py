c84e5c106ee95deac64c3e0b9e6ec8ae203cac47
Axel Jacobsen <axelnj44@gmail.com>
2022-06-27 16:47:10 -0700

    variance -> stddev and LeakyReLU to GELU

--- visualize_trained.py
+++ visualize_trained.py
@@ -29,20 +29,19 @@ def get_confusion_data(net, dataset, sample_size=100, device=torch.device("cpu")
 
     net.eval()
 
-    outputs, variances = [], []
+    outputs, std_devs = [], []
     classes_in_order = sorted([int(c) for c in dataset.classes])
     for clss in classes_in_order:
         samples = dataset.sample_from_class(int(clss), sample_size).to(device)
         with torch.no_grad():
             out = net(samples)
-            output_num = out.mean().item()
-            output_var = out.var(unbiased=True)
+            output_stddev, output_mean = torch.std_mean(out, unbiased=True)
         outputs.append(output_num)
-        variances.append(output_var)
+        std_devs.append(output_stddev)
 
     net.train(prev_net_state)
 
-    return classes_in_order, outputs, variances
+    return classes_in_order, outputs, std_devs
 
 
 if __name__ == "__main__":
@@ -61,17 +60,17 @@ if __name__ == "__main__":
     sample_size = 5
     [full_dataset_subset] = get_dataset("training_data", sample_size)
     full_dataset = full_dataset_subset.dataset
-    classes_in_order, outputs, variances = get_confusion_data(
+    classes_in_order, outputs, std_devs = get_confusion_data(
         net, full_dataset, sample_size=sample_size
     )
     outputs = np.asarray(outputs)
-    variances = np.asarray(variances)
+    std_devs = np.asarray(std_devs)
     plt.scatter(classes_in_order, classes_in_order, s=2)
     plt.scatter(classes_in_order, [np.round(o) for o in outputs], c="red", s=2)
     plt.fill_between(
         classes_in_order,
-        outputs - variances,
-        outputs + variances,
+        outputs - std_devs,
+        outputs + std_devs,
         alpha=0.2,
         edgecolor="#1B2ACC",
         facecolor="#089FFF",
