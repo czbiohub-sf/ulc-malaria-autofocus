d29a5e6c2c505cfb93cd9439b30972d5ef29a330
Axel Jacobsen <axelnj44@gmail.com>
2022-10-25 21:59:23 -0700

    fix model kernel size

--- model.py
+++ model.py
@@ -6,25 +6,27 @@ class AutoFocus(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv_block_1 = nn.Sequential(
-            nn.Conv2d(1, 32, 11, padding=5),
+            nn.Conv2d(1, 32, 5, padding=5),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
             nn.MaxPool2d(5, stride=5),
         )
         self.conv_block_2 = nn.Sequential(
-            nn.Conv2d(32, 32, 7, padding=3),
+            nn.Conv2d(32, 32, 3, padding=3),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
             nn.MaxPool2d(5, stride=2),
         )
         self.conv_block_3 = nn.Sequential(
-            nn.Conv2d(32, 32, 5, padding=2),
+            nn.Conv2d(32, 32, 3, padding=2),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(),
             nn.MaxPool2d(5, stride=2),
         )
         self.conv_block_4 = nn.Sequential(
-            nn.Conv2d(32, 1, 3, padding=1), nn.Flatten(start_dim=1), nn.Linear(204, 1)
+            nn.Conv2d(32, 1, 3, padding=1),
+            nn.Flatten(start_dim=1),
+            nn.Linear(300, 1)
         )
 
     def forward(self, x: torch.Tensor):
