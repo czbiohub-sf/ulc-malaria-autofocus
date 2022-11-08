1ebc21f9506fa570955c6af0ddcd173a57b629c2
Axel Jacobsen <axelnj44@gmail.com>
2022-11-08 13:20:04 -0800

    clean up ssaf_training.sh

--- ssaf_training.sh
+++ ssaf_training.sh
@@ -5,51 +5,21 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=a40:1
+#SBATCH --gpus-per-node=a100:1
 #SBATCH --output=./slurm-outputs/slurm-%j.out
 
 echo
 echo "Single-Shot Auto Focus (SSAF) Training"
 echo
 
-# --gpus-per-node=A100:1
-
 env | grep "^SLURM" | sort
-
-if [[ $# -eq 0 ]]; then
-  cmd="python3 train.py"
-else
-  cmd="$@"
-fi
-
-#echo
-#echo "copying to /tmp/training_data..."
-#echo
-
-# curious about transfer time
-#echo $(date '+%d/%m/%Y %H:%M:%S')
-#mkdir -p /tmp/training_data
-#tar -xf /hpc/projects/flexo/MicroscopyData/Bioengineering/LFM\ Scope/ssaf_trainingdata/2022-06-10-1056/training_data.tar.gz -C /tmp
-#echo $(date '+%d/%m/%Y %H:%M:%S')
-
-#echo
-#echo "transferred"
-#echo
 
 echo
 echo "starting ssaf training..."
 echo
 
-
 nvidia-smi
 
 wandb online
-conda run $cmd
+conda run "$@"
 wandb offline
-
-#echo
-#echo "removing training data..."
-#echo
-
-#rm -rf /tmp/training_data/*
-#rmdir /tmp/training_data
