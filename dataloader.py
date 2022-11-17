081b74db3194012a07522c859166b77144e9cbe8
Axel Jacobsen <axelnj44@gmail.com>
2022-11-16 21:29:02 -0800

    add brightnessjitter

--- dataloader.py
+++ dataloader.py
@@ -13,6 +13,7 @@ from torchvision.transforms import (
     Resize,
     RandomHorizontalFlip,
     RandomVerticalFlip,
+    ColorJitter,
 )
 
 from pathlib import Path
@@ -74,7 +75,7 @@ class ImageFolderWithLabels(datasets.ImageFolder):
 
 def load_dataset_description(
     dataset_description,
-) -> Tuple[List[str], List[Dict[str, Path]], Dict[str, float]]:
+) -> Tuple[List[Path], Dict[str, float]]:
     with open(dataset_description, "r") as desc:
         yaml_data = yaml.safe_load(desc)
 
@@ -146,8 +147,15 @@ def get_datasets(
     ) = load_dataset_description(dataset_description_file)
 
     augmentations = (
-        [RandomHorizontalFlip(0.5), RandomVerticalFlip(0.5)] if training else []
+        [
+            RandomHorizontalFlip(0.5),
+            RandomVerticalFlip(0.5),
+            ColorJitter(brightness=(0.6, 1.2)),
+        ]
+        if training
+        else []
     )
+
     # scale to [0,1]?
     transforms = Compose([Resize([300, 400]), *augmentations])
 
