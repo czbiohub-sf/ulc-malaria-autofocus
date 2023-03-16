65d7a9f7d4ee9c4df33b2962e3e7d0da99c0645b
Michelle Khoo <michelle.khoo@czbiohub.org>
2023-03-16 15:30:31 -0700

    Added output to infer.py

--- infer.py
+++ infer.py
@@ -126,6 +126,7 @@ if __name__ == "__main__":
 
     no_imgs = args.images is None
     no_zarr = args.zarr is None
+    no_output = args.output is None
     if (no_imgs and no_zarr) or (not no_imgs and not no_zarr):
         print("you must supply a value for only one of --images or --zarr")
         sys.exit(1)
@@ -139,6 +140,9 @@ if __name__ == "__main__":
 
     if args.allan_dev:
         calculate_allan_dev(model, image_loader)
-    else:
+    elif no_output:
         for res in infer(model, image_loader):
             print(res)
+    else:
+        with open(args.output, 'w') as file:
+            file.write(infer(model, image_loader))
