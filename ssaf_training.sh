3d589b6854c0cf2d7ec1039308769f66440cc2bd
Axel Jacobsen <axelnj44@gmail.com>
2023-04-18 17:47:40 -0700

    halve cpus-per-task

--- ssaf_training.sh
+++ ssaf_training.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=a40:1
-#SBATCH --cpus-per-task=64
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=32
 #SBATCH --output=./slurm-outputs/slurm-%j.out
 
 echo
