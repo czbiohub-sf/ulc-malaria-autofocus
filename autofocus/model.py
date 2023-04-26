329ce30f88e4a422024ab7f0b8f7a3735bc12eaa
Axel Jacobsen <axelnj44@gmail.com>
2023-04-26 12:56:07 -0700

    tweak model 2

--- autofocus/model.py
+++ autofocus/model.py
@@ -8,28 +8,43 @@ class AutoFocus(nn.Module):
 
         self.model = nn.Sequential(
             nn.Sequential(
-                nn.Conv2d(1, 16, 7, stride=2, padding=5, bias=False),
+                nn.Conv2d(1, 16, 5, padding=5, bias=False),
                 nn.BatchNorm2d(16),
-                nn.GELU(),
+                nn.LeakyReLU(),
+                nn.Conv2d(16, 16, 5, stride=2, bias=False),
             ),
             nn.Sequential(
-                nn.Conv2d(16, 32, 5, stride=2, padding=3, bias=False),
+                nn.Conv2d(16, 32, 3, padding=3, bias=False),
                 nn.BatchNorm2d(32),
-                nn.GELU(),
+                nn.LeakyReLU(),
+                nn.Conv2d(32, 32, 5, stride=2, bias=False),
             ),
             nn.Sequential(
-                nn.Conv2d(32, 64, 5, stride=2, padding=3, bias=False),
-                nn.BatchNorm2d(64),
-                nn.GELU(),
+                nn.Conv2d(32, 32, 3, padding=2, bias=False),
+                nn.BatchNorm2d(32),
+                nn.LeakyReLU(),
+                nn.Conv2d(32, 32, 5, stride=2, bias=False),
             ),
             nn.Sequential(
-                nn.Conv2d(64, 1, 3, stride=2, padding=1),
+                nn.Conv2d(32, 1, 3, padding=1),
                 nn.Flatten(start_dim=1),
-                nn.Linear(520, 256),
-                nn.GELU(),
-                nn.Linear(256, 1),
+                nn.Linear(1850, 512),
+                nn.LeakyReLU(),
+                nn.Linear(512, 1),
             ),
         )
+
+        self.model.apply(self.init_network_weights)
+
+    @staticmethod
+    def init_network_weights(module: nn.Module):
+        if isinstance(module, nn.Conv2d):
+            # init weights to default leaky relu neg slope, biases to 0
+            torch.nn.init.kaiming_normal_(
+                module.weight, a=0.01, nonlinearity="leaky_relu"
+            )
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
 
     def forward(self, x):
         return self.model(x)
