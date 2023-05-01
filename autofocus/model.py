8f692d2e80cecccca6bc7a320c1dac1470c40acc
Axel Jacobsen <axelnj44@gmail.com>
2023-05-01 16:03:47 -0700

    valiant-disco model retained

--- autofocus/model.py
+++ autofocus/model.py
@@ -59,3 +59,45 @@ class AutoFocus(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+class AutoFocusOlder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(1, 16, 5, padding=5),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(5, stride=5),
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, padding=3),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(5, stride=2),
+        )
+        self.conv_block_3 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, padding=2),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(5, stride=2),
+        )
+        self.conv_block_4 = nn.Sequential(
+            nn.Conv2d(32, 1, 3, padding=1), nn.Flatten(start_dim=1), nn.Linear(300, 1)
+        )
+
+    @classmethod
+    def from_pth(cls, path_to_pth_file: Union[str, Path]) -> "AutoFocusOlder":
+        model = cls()
+        checkpoint = torch.load(path_to_pth_file, map_location="cpu")
+        msd = checkpoint["model_state_dict"]
+        model.load_state_dict(msd)
+        return model
+
+    def forward(self, x: torch.Tensor):
+        x = x.float()
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.conv_block_3(x)
+        x = self.conv_block_4(x)
+        return x
